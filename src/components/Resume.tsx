9676f2c03bea0fe90ade12ec946fe9c562eb3a71
AAlchi <alialchi07@gmail.com>
2024-01-30 21:30:25 -0600

    Bug Fixes

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -3,7 +3,7 @@ const Resume = () => {
     <iframe
         title="PDF Viewer"
         style={{ width: '100%', height: '100vh' }}
-        src="/imgs/MainResume.pdf" 
+        src="https://firebasestorage.googleapis.com/v0/b/aliai-9f3e7.appspot.com/o/MainResume.pdf?alt=media&token=935bd77c-7bdd-4594-a72a-cc4c7780b4f3" 
       />
   );
 };
