40f9565cbcb45695fc1511ad6cc765d4f086b294
Ali Alchikh Ibrahim <alialchikhibrahim@Alis-MacBook-Pro.local>
2025-02-17 17:18:24 -0600

    Resume & LinkedIn update

--- pages/components/landing/Landing.tsx
+++ pages/components/landing/Landing.tsx
@@ -58,6 +58,12 @@ const Landing = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
               </svg>
             </Link>
+            <Link target="_blank" className="flex items-center gap-2 cursor-pointer" style={{ textShadow: "1px 1px 1px white, 0 0 1em white, 0 0 0.1em black" }} href="https://www.linkedin.com/in/aliai07">
+              LinkedIn
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" style={{ width: "20px" }}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+              </svg>
+            </Link>
           </div>
         </AnimatedDiv> 
 
